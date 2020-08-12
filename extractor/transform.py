6b2f17f9b1a96710cbabdce59ac9befc3c7ad4a2
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2020-08-12 21:04:28 +0200

    Add functions for transforming data to DTO

--- extractor/transform.py
+++ extractor/transform.py
@@ -2,6 +2,8 @@ import re
 from re import Pattern
 from typing import List
 
+from extractor.dto import VideoDTO
+
 VIDEO_ID_PATTERN = re.compile('embed\/(.*?)"')
 
 
@@ -10,4 +12,34 @@ def extract_video_id(text: str, pattern: Pattern) -> str:
 
 
 def extract_image_sequence(text: str, separator: str) -> List[str]:
-    return text.split(separator)
+    return text.split(separator) if text else []
+
+
+def extract_tags(text: str, separator: str) -> List[str]:
+    return text.split(separator) if text else []
+
+
+def extract_categories(text: str, separator: str) -> List[str]:
+    return text.split(separator) if text else []
+
+
+def extract_actors(text: str, separator: str) -> List[str]:
+    return text.split(separator) if text else []
+
+
+def transform_raw_video_to_video_dto(input: dict) -> VideoDTO:
+    return VideoDTO(
+        id=extract_video_id(input["iframe_video"], VIDEO_ID_PATTERN),
+        main_image_small=input["main_image_thumb"],
+        image_sequence_small=extract_image_sequence(input["image_sequence_thumb"], ";"),
+        title=input["title"],
+        tags=extract_tags(input["tags"], ";"),
+        categories=extract_categories(input["categories"], ";"),
+        actors=extract_actors(input["actors"], ";"),
+        duration=input["duration"],
+        views=input["views"],
+        upvotes=input["upvotes"],
+        downvotes=input["downvotes"],
+        main_image_big=input["main_image"],
+        image_sequence_big=extract_image_sequence(input["image_sequence"], ";"),
+    )
